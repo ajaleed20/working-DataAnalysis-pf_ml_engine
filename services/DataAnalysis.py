fdf201ac063e504c38455770647f703069c7ccd5
ajaleed <ahsan_jaleed17@outlook.com>
2021-01-05 20:05:55 +0500

    stumpy

--- services/DataAnalysis.py
+++ services/DataAnalysis.py
@@ -7,6 +7,8 @@ import config
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+import stumpy
+from matplotlib.patches import Rectangle
 
 def analysis_to_file(res_df,mp_ids,iter_num):
     string_ints = [str(int) for int in mp_ids]
@@ -22,37 +24,85 @@ def threshold_diff(df,mpid,iter_num):
                 df_filter.to_csv('DataAnalysis/' + data_analysis.Filename_Threshold.value + '_threshold_'+ str(threshold)
                                  +'_for_'+df.columns[i]+'_iteration_'+str(iter_num) + '.csv', index=False, header=True)
 
+
+def get_stumpy(df,num):
+    days_dict = {
+        "One-Hour": 120,
+        "3-Hour"  : 360,
+        "6-Hour"  : 720,
+        "9-Hour"  : 1080,
+        "12-Hour" : 1440,
+        #"Half-Day": 24,
+        "1-Day"   : 2880,
+        #"2-Days": 96,
+        #"5-Days": 240,
+        #"7-Days": 336,
+    }
+    # m = 120
+    # mp = stumpy.stump(df[df.columns[1]], m)
+    # print(mp)
+    # plt.plot(df[df.columns[1]])
+    # plt.show()
+    # plt.plot(mp[:, 0])
+    # plt.show()
+    # print(mp[:, 0].min())
+
+
+    #-----------------------------------------------------------------------------
+    m = days_dict['3-Hour']
+    mp = stumpy.stump(df[df.columns[1]], m)
+    fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+    plt.suptitle('Motif (Pattern) Discovery', fontsize='30')
+
+    axs[0].plot(df[df.columns[1]])
+    axs[0].set_ylabel('Flow Valve 008', fontsize='08')
+    #rect = Rectangle((643, 0), m, 40, facecolor='lightgrey')
+    #axs[0].add_patch(rect)
+    #rect = Rectangle((8724, 0), m, 40, facecolor='lightgrey')
+    #axs[0].add_patch(rect)
+    axs[1].set_xlabel('Time', fontsize='20')
+    axs[1].set_ylabel('Matrix Profile', fontsize='08')
+    #axs[1].axvline(x=643, linestyle="dashed")
+    #axs[1].axvline(x=8724, linestyle="dashed")
+    axs[1].plot(mp[:, 0])
+    plt.show()
+    print(mp[:, 0].min())
+
+
+
 def plot_data_analysis_graphs(df,iter):
 
     degrees = 70
-    for i in range(1, len(df.columns)):
-        res_df_ts = df['ts'].values
-        res_df_data = df[df.columns[i]]
-        plt.figure(figsize=(22, 22))
-        plt.plot(res_df_ts, res_df_data, 'r')
-        plt.xlabel('ts',fontsize=24,fontweight="bold")
-        plt.ylabel(str(df.columns[i]),fontsize=24,fontweight="bold")
-        plt.title('Historic Data for '+ str(df.columns[i]), fontsize=24,fontweight="bold")
-        plt.xticks(rotation=degrees,weight = 'bold',fontsize=20)
-        plt.yticks(weight = 'bold',fontsize=20)
-        plt.savefig('Graphs_DataAnalysis/' + 'Analysis_Historic Data_' + 'mp_id_' + str(df.columns[i]) + ' _iteration' + str(iter))
-        plt.show()
 
-    for i in range(2, len(df.columns)):
-        x = df[df.columns[1]]
-        y = df[df.columns[i]]
-        print(np.corrcoef(x, y))
-        plt.figure(figsize=(22, 22))
-        plt.scatter(x, y)
-        plt.title('A plot to show the correlation between_' + str(df.columns[1]) + '_and_' + str(df.columns[i]),fontsize=24,fontweight="bold")
-        plt.xlabel(str(df.columns[1]),fontsize=24,fontweight="bold")
-        plt.ylabel(str(df.columns[i]),fontsize=24,fontweight="bold")
-        plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), color='red')
-        plt.xticks(rotation=degrees, weight = 'bold',fontsize=20)
-        plt.yticks(weight = 'bold',fontsize=20)
-        #plt.figure(figsize=(10,10))
-        plt.savefig('Graphs_DataAnalysis/' + 'Analysis_Correlated_Data_' + 'mp_id_' + str(df.columns[1]) + '_' + str(df.columns[i]) + ' _iteration' + str(iter))
-        plt.show()
+    if(df.shape[0]>0):
+        for i in range(1, len(df.columns)):
+            res_df_ts = df['ts'].values
+            res_df_data = df[df.columns[i]]
+            plt.figure(figsize=(22, 22))
+            plt.plot(res_df_ts, res_df_data, 'r')
+            plt.xlabel('ts',fontsize=24,fontweight="bold")
+            plt.ylabel(str(df.columns[i]),fontsize=24,fontweight="bold")
+            plt.title('Historic Data for '+ str(df.columns[i]), fontsize=24,fontweight="bold")
+            plt.xticks(rotation=degrees,weight = 'bold',fontsize=20)
+            plt.yticks(weight = 'bold',fontsize=20)
+            plt.savefig('Graphs_DataAnalysis/' + 'Analysis_Historic Data_' + 'mp_id_' + str(df.columns[i]) + ' _iteration' + str(iter))
+            plt.show()
+
+        for i in range(2, len(df.columns)):
+            x = df[df.columns[1]]
+            y = df[df.columns[i]]
+            print(np.corrcoef(x, y))
+            plt.figure(figsize=(22, 22))
+            plt.scatter(x, y)
+            plt.title('A plot to show the correlation between_' + str(df.columns[1]) + '_and_' + str(df.columns[i]),fontsize=24,fontweight="bold")
+            plt.xlabel(str(df.columns[1]),fontsize=24,fontweight="bold")
+            plt.ylabel(str(df.columns[i]),fontsize=24,fontweight="bold")
+            plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)), color='red')
+            plt.xticks(rotation=degrees, weight = 'bold',fontsize=20)
+            plt.yticks(weight = 'bold',fontsize=20)
+            #plt.figure(figsize=(10,10))
+            plt.savefig('Graphs_DataAnalysis/' + 'Analysis_Correlated_Data_' + 'mp_id_' + str(df.columns[1]) + '_' + str(df.columns[i]) + ' _iteration' + str(iter))
+            plt.show()
 
 
 
@@ -72,15 +122,20 @@ def get_mp_data_data_analysis(start_period, end_period, mp_ids, level, iter_num,
     res_df = res_df.fillna(method='ffill')
     res_df.dropna(inplace=True)
 
+    get_stumpy(res_df, iter_num)
+
     plot_data_analysis_graphs(res_df,iter_num)
 
     for i in range(1,len(res_df.columns)-1):
         res_df['diff_FlowValve008_1492 - ' + str(res_df.columns[i + 1])] = res_df[res_df.columns[1]] - res_df[res_df.columns[i + 1]]
 
 
+
     analysis_to_file(res_df,mp_ids,iter_num)
 
     threshold_diff(res_df,mp_ids,iter_num)
+
+
 
 
 def execute_data_analysis(mpid_var, start_period, end_period,granularity_level,iter_num):
