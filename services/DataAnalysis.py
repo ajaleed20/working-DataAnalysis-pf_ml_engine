4da7ae34312885d95f7f763cfcd431a530dec0ce
ajaleed <ahsan_jaleed17@outlook.com>
2021-01-14 19:37:56 +0500

    pattern new technique

--- services/DataAnalysis.py
+++ services/DataAnalysis.py
@@ -20,7 +20,7 @@ def threshold_diff(df,mpid):
         for i in range(len(mpid)+1, len(df.columns)):
             df_filter = df.loc[abs(df[df.columns[i]]) > threshold]
             if(df_filter.shape[0] >  0):
-                df_filter.to_csv('DataAnalysis/' + data_analysis.Filename_Threshold.value + '_threshold_'+ str(threshold)
+                df_filter.to_csv('DataAnalysis/DataAnalysis/' + data_analysis.Filename_Threshold.value + '_threshold_'+ str(threshold)
                                  +'_for_'+df.columns[i]+ '_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.csv', index=False, header=True)
 
 def plot_data_analysis_graphs(df):
