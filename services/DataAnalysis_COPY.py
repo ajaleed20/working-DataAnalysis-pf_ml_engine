c92ca1e6023fd974fa546f470043a84cde45ae24
ajaleed <ahsan_jaleed17@outlook.com>
2021-03-10 14:26:21 +0100

    get_dates func implementation for fetching 15 days date ranges out of total range provided, i.e. last 9 months

--- services/DataAnalysis_COPY.py
+++ services/DataAnalysis_COPY.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from datetime import date, timedelta
 
 
-def analysis_to_file(res_df,mp_ids):
+def analysis_to_file(res_df,mp_ids,df_fetched_dates):
     string_ints = [str(int) for int in mp_ids]
     str_of_mpids = "_".join(string_ints)
     res_df.to_csv('DataAnalysis/DataAnalysis/'+ 'mpid_' + str_of_mpids + data_analysis.Filename_Analysis.value +  '_createdon_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.csv', index = False, header=True)
@@ -57,31 +57,52 @@ def plot_data_analysis_graphs(df):
         plt.show()
 
 
-def get_mp_data_data_analysis(start_period, end_period, mp_ids, level, include_missing_mp= False):
-    dfs, missing_mp_ids = data_service.get_data_by_ids_period_and_level(start_period, end_period, mp_ids,
-                                                                                      level, include_missing_mp)
-    res_df = pd.DataFrame({'ts': pd.date_range(start=start_period, end=end_period, freq=get_freq_by_level(level))})
-
-    res_df['ts'] = res_df['ts'].dt.tz_localize('UTC').dt.tz_convert('Europe/Berlin')
-
-    for df in dfs:
-        res_df = pd.merge(res_df, df, on='ts', how='outer')
-
-    res_df.sort_values(by=['ts'], inplace=True)
-
-    res_df = res_df.drop_duplicates().reset_index(drop=True)
-    res_df = res_df.fillna(method='ffill')
-    res_df.dropna(inplace=True)
-
-    #plot_data_analysis_graphs(res_df)
-
-    #for i in range(1,len(res_df.columns)-1):
-    #    res_df['diff_FlowValve008_1492 - ' + str(res_df.columns[i + 1])] = res_df[res_df.columns[1]] - res_df[res_df.columns[i + 1]]
+def get_mp_data_data_analysis(start_period, end_period, mp_ids, level,df_fetched_dates, include_missing_mp= False):
+    for index, row in df_fetched_dates.iterrows():
+        dfs, missing_mp_ids = data_service.get_data_by_ids_period_and_level(row['start_date'], row['end_date'], mp_ids, level, include_missing_mp)
+        res_df = pd.DataFrame({'ts': pd.date_range(start=row['end_date'], end=row['end_date'], freq=get_freq_by_level(level))})
+        res_df['ts'] = res_df['ts'].dt.tz_localize('UTC').dt.tz_convert('Europe/Berlin')
+        for df in dfs:
+            res_df = pd.merge(res_df, df, on='ts', how='outer')
+        res_df.sort_values(by=['ts'], inplace=True)
+        res_df = res_df.drop_duplicates().reset_index(drop=True)
+        res_df = res_df.fillna(method='ffill')
+        res_df.dropna(inplace=True)
+        analysis_to_file(res_df, mp_ids, df_fetched_dates)
+        print(row['start_date'], row['end_date'])
 
 
-    analysis_to_file(res_df,mp_ids)
 
-    #threshold_diff(res_df,mp_ids)
+
+
+
+
+
+    #
+    # dfs, missing_mp_ids = data_service.get_data_by_ids_period_and_level(start_period, end_period, mp_ids,
+    #                                                                                   level, include_missing_mp)
+    # res_df = pd.DataFrame({'ts': pd.date_range(start=start_period, end=end_period, freq=get_freq_by_level(level))})
+    #
+    # res_df['ts'] = res_df['ts'].dt.tz_localize('UTC').dt.tz_convert('Europe/Berlin')
+    #
+    # for df in dfs:
+    #     res_df = pd.merge(res_df, df, on='ts', how='outer')
+    #
+    # res_df.sort_values(by=['ts'], inplace=True)
+    #
+    # res_df = res_df.drop_duplicates().reset_index(drop=True)
+    # res_df = res_df.fillna(method='ffill')
+    # res_df.dropna(inplace=True)
+    #
+    # #plot_data_analysis_graphs(res_df)
+    #
+    # #for i in range(1,len(res_df.columns)-1):
+    # #    res_df['diff_FlowValve008_1492 - ' + str(res_df.columns[i + 1])] = res_df[res_df.columns[1]] - res_df[res_df.columns[i + 1]]
+    #
+    #
+    # analysis_to_file(res_df,mp_ids,df_fetched_dates)
+    #
+    # #threshold_diff(res_df,mp_ids)
 
 def get_dates(sdate,edate):
     thisdict = {}
@@ -110,8 +131,8 @@ def get_dates(sdate,edate):
 
     return dataframe
 
-def execute_data_analysis(mpid_var, start_period, end_period,granularity_level):
-   get_mp_data_data_analysis(start_period, end_period, mpid_var, granularity_level)
+def execute_data_analysis(mpid_var, start_period, end_period,granularity_level,df_fetched_dates):
+   get_mp_data_data_analysis(start_period, end_period, mpid_var, granularity_level,df_fetched_dates)
 
 
 #instance = data_analysis.instance.value
@@ -119,7 +140,7 @@ config_oauth(config.get_current_config())
 
 try:
     df_fetched_dates = get_dates(data_analysis.start_period.value, data_analysis.end_period.value)
-    execute_data_analysis(data_analysis.measuringpoint_var.value, data_analysis.start_period.value, data_analysis.end_period.value,data_analysis.granularity.value)
+    execute_data_analysis(data_analysis.measuringpoint_var.value, data_analysis.start_period.value, data_analysis.end_period.value,data_analysis.granularity.value,df_fetched_dates)
     print("\nend of program successful with file saved to DataAnalysis and Graphs folders, inside project folder:services")
     exit()
 except Exception as e:
